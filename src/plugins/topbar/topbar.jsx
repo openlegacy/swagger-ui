a57dbef9f5c6697a5430d1935d219739945a890c
Maor ZIlberg <maor.z@openlegacy.com>
2020-05-06 16:22:20 +0300

    OL-8953 remove generate-client button and related styles

--- src/plugins/topbar/topbar.jsx
+++ src/plugins/topbar/topbar.jsx
@@ -146,36 +146,6 @@ export default class Topbar extends React.Component {
             <form className="download-url-wrapper" onSubmit={formOnSubmit}>
               {control.map((el, i) => cloneElement(el, { key: i }))}
             </form>
-            <div className="input">
-              <ul className="generate-client-menu dropit">
-                <li className="dropit-trigger"><a id="client" href="#" className="dropdown-trigger gen-btn">Generate Client ▾</a>
-                  <ul id="menu" className="dropit-submenu" style={{ display: "none" }}>
-                    <li><a href="swagger/clients/akka-scala">Akka Scala</a></li>
-                    <li><a href="swagger/clients/android">Android</a></li>
-                    <li><a href="swagger/clients/async-scala">Async Scala</a></li>
-                    <li><a href="swagger/clients/clojure">Clojure</a></li>
-                    <li><a href="swagger/clients/csharp">C#</a></li>
-                    <li><a href="swagger/clients/CsharpDotNet2">C# .NET 2.0</a></li>
-                    <li><a href="swagger/clients/dart">Dart</a></li>
-                    <li><a href="swagger/clients/flash">Flash</a></li>
-                    <li><a href="swagger/clients/go">Go</a></li>
-                    <li><a href="swagger/clients/java">Java</a></li>
-                    <li><a href="swagger/clients/javascript">Javascript</a></li>
-                    <li><a href="swagger/clients/javascript-closure-angular">Javascript Closure Angular</a></li>
-                    <li><a href="swagger/clients/jmeter">Jmeter</a></li>
-                    <li><a href="swagger/clients/objc">Objective-C</a></li>
-                    <li><a href="swagger/clients/perl">Perl</a></li>
-                    <li><a href="swagger/clients/php">PHP</a></li>
-                    <li><a href="swagger/clients/python">Python</a></li>
-                    <li><a href="swagger/clients/qt5cpp">Qt 5 C++</a></li>
-                    <li><a href="swagger/clients/ruby">Ruby</a></li>
-                    <li><a href="swagger/clients/scala">Scala</a></li>
-                    <li><a href="swagger/clients/swift">Swift</a></li>
-                    <li><a href="swagger/clients/tizen">Tizen</a></li>
-                    <li><a href="swagger/clients/typescript-angular">Typescript Angular</a></li>
-                    <li><a href="swagger/clients/typescript-node">Typescript Node</a></li></ul></li>
-              </ul>
-            </div>
           </div>
         </div>
       </div>
